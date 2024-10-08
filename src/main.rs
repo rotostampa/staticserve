44a2579926a5ba7c5b699b362e83d91864ff49eb
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2024-10-08 19:00:13 +0200

    fix for 404 response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_files as fs;
 use actix_web::http::header::ContentEncoding;
-use actix_web::{get, App, Error, HttpRequest, HttpServer, Result};
+use actix_web::{get, App, Error, HttpRequest, HttpResponse, HttpServer, Result};
 use clap::Parser;
 use std::io::Read;
 use std::path::{Path, PathBuf};
@@ -18,23 +18,36 @@ struct Args {
     #[arg(short, long, default_value = "./files")]
     folder: String,
 
-    /// Directory where the files are served from
+    /// Host to bind the server to
     #[arg(short, long, default_value = "127.0.0.1")]
     host: String,
 }
 
 #[get("/{filename:.*}")]
-async fn index(req: HttpRequest, data: actix_web::web::Data<Args>) -> Result<fs::NamedFile, Error> {
-    let filename: PathBuf = Path::new(&data.folder).join(req.match_info().query("filename"));
+async fn index(req: HttpRequest, data: actix_web::web::Data<Args>) -> Result<HttpResponse, Error> {
+    let filename: PathBuf = if req.match_info().query("filename").is_empty() {
+        PathBuf::new() // Root path "/"
+    } else {
+        Path::new(&data.folder).join(req.match_info().query("filename"))
+    };
 
+    // Check if the path is a directory or does not exist
+    if filename.as_os_str().is_empty() || filename.is_dir() || !filename.exists() {
+        // Return a custom 404 HTML response if it's the root, a directory, or the file does not exist
+        return Ok(HttpResponse::NotFound().content_type("text/html").body(""));
+    }
+
+    // If the file exists, open it
     let mut file = fs::NamedFile::open(filename)?;
 
-    let mut buffer = [0; 2]; // Read the first two bytes
+    // Read the first two bytes
+    let mut buffer = [0; 2];
     if file.read_exact(&mut buffer).is_ok() && buffer == GZIP_MAGIC_BYTES {
         file = file.set_content_encoding(ContentEncoding::Gzip);
     }
 
-    Ok(file.use_last_modified(true))
+    // Return the file as an HttpResponse
+    Ok(file.use_last_modified(true).into_response(&req))
 }
 
 #[actix_web::main]
@@ -43,7 +56,6 @@ async fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
     // Start the HTTP server with the given port and serve files from the provided directory
-
     let port = args.port;
     let host = args.host.clone();
 
